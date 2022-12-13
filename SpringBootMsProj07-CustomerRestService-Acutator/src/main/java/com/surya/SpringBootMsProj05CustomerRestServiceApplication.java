9ad0411534c61cf4abb5b41b3700423f993d31f5
surya9014 <suryanarayana13271@gmail.com>
2022-12-13 14:22:39 +0530

    MicroService Actuators2

---
+++ SpringBootMsProj07-CustomerRestService-Acutator/src/main/java/com/surya/SpringBootMsProj05CustomerRestServiceApplication.java
@@ -0,0 +1,14 @@
+package com.surya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@EnableEurekaClient
+public class SpringBootMsProj05CustomerRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMsProj05CustomerRestServiceApplication.class, args);
+	}
+
+}
